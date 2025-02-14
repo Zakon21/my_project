b42b111548dbc433cc59f985c627e3be1d4d6070
Zakon21 <git@github.com:Zakon21>
2025-02-14 15:03:37 +0300

    with new tables

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from quart import Quart, jsonify, request
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, create_async_engine
 from sqlalchemy.future import select
-from sqlalchemy.orm import sessionmaker, declarative_base,relationship
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship
 from sqlalchemy import Column, Integer, String, Float, DateTime, TIMESTAMP, TEXT, BOOLEAN, ForeignKey
 from dotenv import load_dotenv, find_dotenv
 import os
@@ -22,19 +22,60 @@ async_session = sessionmaker(
 class House(Base):
     __tablename__ = 'houses'
     id = Column(Integer, primary_key = True)
-    address = Column(String(255), nullable = False)
+    address = Column(String, nullable = False)
     date = Column(TIMESTAMP, nullable = False)
     area = Column(Float, nullable = False)
     tariff = Column(Float, nullable = False)
+    house_id = Column(Integer, nullable = False)
+
+    contracts = relationship('Contract', back_populates = 'house')
+    protocols = relationship('Protocol', back_populates = 'house')
+    documents = relationship('Document', back_populates = 'house')
     
-class News(Base):
+class New(Base):
     __tablename__ = 'news'
     id = Column(Integer, primary_key = True)
     title = Column(TEXT, nullable = False)
     date = Column(TIMESTAMP, nullable = False)
-    image = Column(String(255), nullable = False)
+    image = Column(String, nullable = False)
     report = Column(BOOLEAN, nullable = False)
     txt = Column(TEXT, nullable = False)
+    report_id = Column(Integer, nullable = False)
+
+class Report(Base):
+    __tablename__ = 'reports'
+    id = Column(Integer, primary_key = True)
+    txt = Column(TEXT, nullable = False)
+    image = Column(String, nullable = False)
+    report_id = Column(Integer, nullable = False) 
+
+class Contract(Base):
+    __tablename__ = 'contracts'
+    id = Column(Integer, primary_key = True, nullable = False)
+    txt = Column(TEXT, nullable = False)
+    house_id = Column(Integer, ForeignKey('houses.id', ondelete = 'CASCADE'), nullable = False)
+
+    house = relationship('House', back_populates = 'contracts')
+class Protocol(Base):
+    __tablename__ = 'protocols'
+    id = Column(Integer, primary_key = True, nullable = False)
+    name = Column(String, nullable = False)
+    number = Column(Integer, nullable = False)
+    date = Column(TIMESTAMP, nullable = False)
+    link = Column(String, nullable = False)
+    house_id = Column(Integer, ForeignKey('houses.id', ondelete = 'CASCADE'), nullable = False)
+
+    house = relationship('House', back_populates = 'protocols')
+class Document(Base):
+    __tablename__ = 'documents'
+    id = Column(Integer, primary_key = True, nullable = False)
+    name = Column(String, nullable = False)
+    number = Column(Integer, nullable = False)
+    date = Column(TIMESTAMP, nullable = False)
+    link = Column(String, nullable = False)
+    house_id = Column(Integer, ForeignKey('houses.id', ondelete = 'CASCADE'), nullable = False)
+
+    house = relationship('House', back_populates = 'documents')
 
 @app.route('/houses', methods = ['GET'])
 async def get_houses():
@@ -56,21 +97,87 @@ async def get_houses():
 @app.route('/news', methods = ['GET'])
 async def get_news():
     async with async_session() as session:
-        result = await session.execute(select(News))
-        newses = result.scalars().all()
-        newses_list = [
+        result = await session.execute(select(New).order_by(New.date.desc()))
+        news = result.scalars().all()
+        news_list = [
             {
-                'id': news.id,
-                'title': news.title,
-                'date': news.date.strftime('%d.%m.%Y'),
-                'image': news.image,
-                'report': news.report,
-                'txt': news.txt 
+                'id': new.id,
+                'title': new.title,
+                'date': new.date.strftime('%d.%m.%Y'),
+                'image': new.image,
+                'report': new.report,
+                'txt': new.txt 
             }
-            for news in newses
+            for new in news
         ]
-        return jsonify(newses_list)
+        return jsonify(news_list)
 
+@app.route('/contracts', methods = ['GET'])
+async def get_contracts():
+    async with async_session() as session:
+        result = await session.execute(select(Contract))
+        contracts = result.scalars().all()
+        contracts_list = [
+            {
+                'id': contract.id,
+                'txt': contract.txt,
+                'house_id': contract.house_id
+            }
+            for contract in contracts
+        ]
+        return jsonify(contracts_list)
+    
+@app.route('/protocols', methods = ['GET'])
+async def get_protocols():
+    async with async_session() as session:
+        result = await session.execute(select(Protocol))
+        protocols = result.scalars().all()
+        protocols_list = [
+            {
+                'id': protocol.id,
+                'name': protocol.name,
+                'number': protocol.number,
+                'date': protocol.date.strftime('%d.%m.%Y'),
+                'house_id': protocol.house_id,
+                'link': protocol.link
+            }
+            for protocol in protocols
+        ]
+        return jsonify(protocols_list)
+
+@app.route('/documents', methods = ['GET'])
+async def get_documents():
+    async with async_session() as session:
+        result = await session.execute(select(Document))
+        documents = result.scalars().all()
+        documents_list = [
+            {
+                'id': document.id,
+                'name': document.name,
+                'number': document.number,
+                'date': document.date.strftime('%d.%m.%Y'),
+                'link': document.link,
+                'house_id': document.house_id
+            }
+            for document in documents
+        ]
+        return jsonify(documents_list)
+
+@app.route('/reports', methods = ['GET'])
+async def get_reports():
+    async with async_session() as session:
+        result = await session.execute(select(Report))
+        reports = result.scalars().all()
+        reports_list = [
+            {
+                'id': report.id,
+                'txt': report.txt,
+                'image': report.image,
+                'report_id': report.report_id
+            }
+            for report in reports
+        ]
+        return jsonify(reports_list)
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 5000)
